99195b594b50fad514773dc79dbee349fe1c12bd
maksim <guz42@bk.ru>
2026-06-14 17:53:19 +0700

    v.1

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
@@ -0,0 +1,56 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF1A1B22)
+val White = Color(0xFFFDFDFD)
+
+val Blue = Color(0xFF3772E7)
+val Red = Color(0xFFF56B6C)
+
+val Grey = Color(0xFFAEAFB4)
+val LightGrey = Color(0xFFE6E8EB)
+
+val OverlayBackground = Color(0x801A1B22)
+
+// Light Theme
+val LightPrimary = Blue
+val LightOnPrimary = White
+
+val LightSecondary = Grey
+val LightOnSecondary = Black
+
+val LightBackground = White
+val LightOnBackground = Black
+
+val LightSurface = White
+val LightOnSurface = Black
+
+val LightSurfaceVariant = LightGrey
+val LightOnSurfaceVariant = Grey
+
+val LightSurfaceContainer = Color(0xFFF1F3F5)
+val LightInversionOnSurface = Black
+
+val LightError = Red
+
+// Dark Theme
+val DarkPrimary = Blue
+val DarkOnPrimary = White
+
+val DarkSecondary = Grey
+val DarkOnSecondary = White
+
+val DarkBackground = Black
+val DarkOnBackground = White
+
+val DarkSurface = Black
+val DarkOnSurface = White
+
+val DarkSurfaceVariant = Color(0xFF2A2C35)
+val DarkOnSurfaceVariant = Grey
+
+val DarkSurfaceContainer = Color(0xFF2A2C35)
+val DarkInversionOnSurface = White
+
+val DarkError = Red
